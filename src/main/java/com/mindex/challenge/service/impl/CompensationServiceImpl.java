a152eeb98175f3afd1456654610ca33f9c067761
Kirtana <ks3057@rit.edu>
2021-03-11 04:13:00 -0500

    Implemented compensation create

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -2,7 +2,9 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.service.CompensationService;
+import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,21 @@ public class CompensationServiceImpl implements CompensationService {
     @Autowired
     private CompensationRepository compensationRepository;
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @Override
     public Compensation create(Compensation compensation) {
+        String employeeId = compensation.getEmployee().getEmployeeId();
+        Employee employee = employeeService.read(employeeId);
+        //get the updated employee from database
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + employeeId);
+        }
+
+        compensation.setEmployee(employee);
         compensationRepository.insert(compensation);
-        return null;
+        return compensation;
     }
 
     @Override
